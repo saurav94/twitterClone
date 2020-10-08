64ed41b9ad2bd6d278fe7f25de128042c071196e
saurav94 <patel.saurav.b@gmail.com>
2020-10-08 23:34:53 +0530

    First commit

---
+++ tweet/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+class Tweet(models.Model):
+    text = models.TextField()
+    date_posted = models.DateTimeField(default = timezone.now)
+    author =  models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
