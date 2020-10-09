ae199654d1d48b17c7fbcf61969251a812a2165d
saurav94 <patel.saurav.b@gmail.com>
2020-10-09 12:28:06 +0530

    adding secret file

--- twitterClone/settings.py
+++ twitterClone/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from .secrets import secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'aa4c$m9^=ix=41w+qw*b)who&l9zgx4v_lr5a8g==qw89()x@e'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
