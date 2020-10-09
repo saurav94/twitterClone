36ca579746d2aee7efe2b36cef52d75ecd7f20ba
saurav94 <patel.saurav.b@gmail.com>
2020-10-09 23:06:39 +0530

    user registration

--- twitterClone/settings.py
+++ twitterClone/settings.py
@@ -32,7 +32,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'tweet.apps.TweetConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
