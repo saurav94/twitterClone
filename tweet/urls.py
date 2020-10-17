399514b3601e14d7bd8f7088a499b1070bdbba4d
saurav94 <patel.saurav.b@gmail.com>
2020-10-17 17:13:58 +0530

    user tweets and pagination

--- tweet/urls.py
+++ tweet/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import TweetListView, TweetDetailView, TweetCreateView, TweetUpdateView, TweetDeleteView, about
+from .views import TweetListView, UserTweetListView, TweetDetailView, TweetCreateView, TweetUpdateView, TweetDeleteView, about
 
 urlpatterns = [
     path('', TweetListView.as_view(), name='tweet-home'),
+    path('user/<str:username>', UserTweetListView.as_view(), name='user-tweets'),
     path('about/', about, name='tweet-about'),
     path('tweet/<int:pk>', TweetDetailView.as_view(), name='tweet-detail'),
     path('tweet/new/', TweetCreateView.as_view(), name='tweet-create'),
--- tweet/views.py
+++ tweet/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Tweet
@@ -12,6 +12,18 @@ class TweetListView(ListView):
     template_name = 'tweet/home.html' #<app>/<model>_<viewtype>.html
     context_object_name = 'tweets'
     ordering = ['-date_posted']
+    paginate_by = 5
+
+
+class UserTweetListView(ListView):
+    model = Tweet
+    template_name = 'tweet/user_tweets.html'
+    context_object_name = 'tweets'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Tweet.objects.filter(author=user).order_by('-date_posted')
 
 
 class TweetDetailView(DetailView):
